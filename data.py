edad9c5f0320b8eefc43f11407eb5d9d8dd8bf56
Yi-Chia Chang <yichia3@illinois.edu>
2024-10-05 00:49:37 -0500

    Update dataset and run.py

--- data.py
+++ data.py
@@ -1,42 +1,8 @@
 import lightning as pl
-import torch
 from torch.utils.data import DataLoader, Dataset
 import pandas as pd
-# from data_process import importdata
-import data_process as data
-import json
 
-
-with open("./config.json") as f:
-    config=json.load(f)
-
-device = torch.device("cuda" if config['training']['gpu'] and (torch.cuda.is_available()) else "cpu")
-
-datadir='datasets/'+config['circuit']['dev']+'/'+config['circuit']['sel']+'/'
-rawdata = data.importdata(path=datadir, sel=config['circuit']['dev'],ni=config['circuit']['ninput'], \
-                          no=config['circuit']['noutput'],aging=config['circuit']['aging'], \
-                          agestep=config['circuit']['agestep'], unistep=config['circuit']['unistep'], \
-                          maxsample=2000,tscale=config['circuit']['hRNN'])
-
-if config['circuit']['unistep']:
-    for key in rawdata:
-        for i in range(len(rawdata[key])):
-            if len(rawdata[key][i])>config['circuit']['nstep']:
-                rawdata[key][i]=rawdata[key][i][0:config['circuit']['nstep']]
-
-#Outputs must be normalized if cross-entropy loss is used:
-if config['training']['loss']=='CE':
-    for i in range(config['circuit']['noutput']):
-        if str(i) not in config['model']['normoutputs']:
-            config['model']['normoutputs'][str(i)]=[]
-    if config['model']['outclip'] is None:
-        config['model']['outclip']='sig_cust'
-rawdata['inset'],config['model']['norminputs']=data.normalize(rawdata['inset'], config['model']['norminputs'])
-rawdata['outset'],config['model']['normoutputs']=data.normalize(rawdata['outset'], config['model']['normoutputs'])
-
-train_dataset, val_dataset= data.split(rawdata,rate=0.8,shuffle=False,aging=config['circuit']['aging'], \
-                              agestep=config['circuit']['agestep'])
-
+# Need to add in_len and out_len in the dataset, see data/data_loader.py
 class CircuitDataset(Dataset):
     def __init__(self, dataset):
         self.dataset=dataset
@@ -70,7 +36,3 @@ class CircuitDataModule(pl.LightningDataModule):
 
     def val_dataloader(self):
         return DataLoader(self.valid_dataset, batch_size=self.batch_size, num_workers=self.num_workers)
-
-
-data_module = CircuitDataModule(train_dataset=train_dataset, val_dataset=val_dataset, batch_size=32, num_workers=4)
-print('done')
