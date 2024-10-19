71011de7250e3ba409f210dd0702882398fc1a97
Yi-Chia Chang <yichia3@illinois.edu>
2024-10-18 22:14:33 -0500

    circuit datamodule

--- cross_models/cross_former.py
+++ cross_models/cross_former.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from einops import rearrange, repeat
+import lightning as pl
 
 from cross_models.cross_encoder import Encoder
 from cross_models.cross_decoder import Decoder
@@ -10,9 +11,9 @@ from cross_models.cross_embed import DSW_embedding
 
 from math import ceil
 
-class Crossformer(nn.Module):
+class Crossformer(pl.LightningModule):
     def __init__(self, data_dim, in_len, out_len, seg_len, win_size = 4,
-                factor=10, d_model=512, d_ff = 1024, n_heads=8, e_layers=3, 
+                factor=10, d_model=512, d_ff = 1024, n_heads=8, e_layers=3,
                 dropout=0.0, baseline = False, device=torch.device('cuda:0')):
         super(Crossformer, self).__init__()
         self.data_dim = data_dim
@@ -23,7 +24,7 @@ class Crossformer(nn.Module):
 
         self.baseline = baseline
 
-        self.device = device
+        # self.device = device
 
         # The padding operation to handle invisible sgemnet length
         self.pad_in_len = ceil(1.0 * in_len / seg_len) * seg_len
@@ -38,12 +39,12 @@ class Crossformer(nn.Module):
         # Encoder
         self.encoder = Encoder(e_layers, win_size, d_model, n_heads, d_ff, block_depth = 1, \
                                     dropout = dropout,in_seg_num = (self.pad_in_len // seg_len), factor = factor)
-        
+
         # Decoder
         self.dec_pos_embedding = nn.Parameter(torch.randn(1, data_dim, (self.pad_out_len // seg_len), d_model))
         self.decoder = Decoder(seg_len, e_layers + 1, d_model, n_heads, d_ff, dropout, \
                                     out_seg_num = (self.pad_out_len // seg_len), factor = factor)
-        
+
     def forward(self, x_seq):
         if (self.baseline):
             base = x_seq.mean(dim = 1, keepdim = True)
@@ -56,7 +57,7 @@ class Crossformer(nn.Module):
         x_seq = self.enc_value_embedding(x_seq)
         x_seq += self.enc_pos_embedding
         x_seq = self.pre_norm(x_seq)
-        
+
         enc_out = self.encoder(x_seq)
 
         dec_in = repeat(self.dec_pos_embedding, 'b ts_d l d -> (repeat b) ts_d l d', repeat = batch_size)
@@ -64,3 +65,21 @@ class Crossformer(nn.Module):
 
 
         return base + predict_y[:, :self.out_len, :]
+
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self(x)
+        loss = F.mse_loss(y_hat, y)
+        self.log('train_loss', loss)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self(x)
+        loss = F.mse_loss(y_hat, y)
+        self.log('val_loss', loss)
+        return loss
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
+        return optimizer
--- run.py
+++ run.py
@@ -1,55 +1,63 @@
-import pytorch_lightning as pl
-from pytorch_lightning import Trainer
-from cross_models.cross_former import Crossformer
+import lightning as pl
+from lightning.pytorch import Trainer
+# from cross_models.cross_former import Crossformer
+from cross_models.crossformer_pl import CrossformerLightningModule
+
 from data import CircuitDataModule
 import torch
-import data_process as data
+import Previous_code_WY.data_process as data
 import json
 
+if __name__ == "__main__":
 
-with open("./config.json") as f:
-    config=json.load(f)
+    with open("./config.json") as f:
+        config=json.load(f)
 
-device = torch.device("cuda" if config['training']['gpu'] and (torch.cuda.is_available()) else "cpu")
+    device = torch.device("cuda" if config['training']['gpu'] and (torch.cuda.is_available()) else "cpu")
 
-datadir='datasets/'+config['circuit']['dev']+'/'+config['circuit']['sel']+'/'
-rawdata = data.importdata(path=datadir, sel=config['circuit']['dev'],ni=config['circuit']['ninput'], \
-                          no=config['circuit']['noutput'],aging=config['circuit']['aging'], \
-                          agestep=config['circuit']['agestep'], unistep=config['circuit']['unistep'], \
-                          maxsample=2000,tscale=config['circuit']['hRNN'])
+    datadir='datasets/'+config['circuit']['dev']+'/'+config['circuit']['sel']+'/'
+    rawdata = data.importdata(path=datadir, sel=config['circuit']['dev'],ni=config['circuit']['ninput'], \
+                            no=config['circuit']['noutput'],aging=config['circuit']['aging'], \
+                            agestep=config['circuit']['agestep'], unistep=config['circuit']['unistep'], \
+                            maxsample=2000,tscale=config['circuit']['hRNN'])
 
-if config['circuit']['unistep']:
-    for key in rawdata:
-        for i in range(len(rawdata[key])):
-            if len(rawdata[key][i])>config['circuit']['nstep']:
-                rawdata[key][i]=rawdata[key][i][0:config['circuit']['nstep']]
+    if config['circuit']['unistep']:
+        for key in rawdata:
+            for i in range(len(rawdata[key])):
+                if len(rawdata[key][i])>config['circuit']['nstep']:
+                    rawdata[key][i]=rawdata[key][i][0:config['circuit']['nstep']]
 
-#Outputs must be normalized if cross-entropy loss is used:
-if config['training']['loss']=='CE':
-    for i in range(config['circuit']['noutput']):
-        if str(i) not in config['model']['normoutputs']:
-            config['model']['normoutputs'][str(i)]=[]
-    if config['model']['outclip'] is None:
-        config['model']['outclip']='sig_cust'
-rawdata['inset'],config['model']['norminputs']=data.normalize(rawdata['inset'], config['model']['norminputs'])
-rawdata['outset'],config['model']['normoutputs']=data.normalize(rawdata['outset'], config['model']['normoutputs'])
+    #Outputs must be normalized if cross-entropy loss is used:
+    if config['training']['loss']=='CE':
+        for i in range(config['circuit']['noutput']):
+            if str(i) not in config['model']['normoutputs']:
+                config['model']['normoutputs'][str(i)]=[]
+        if config['model']['outclip'] is None:
+            config['model']['outclip']='sig_cust'
+    rawdata['inset'],config['model']['norminputs']=data.normalize(rawdata['inset'], config['model']['norminputs'])
+    rawdata['outset'],config['model']['normoutputs']=data.normalize(rawdata['outset'], config['model']['normoutputs'])
 
-train_dataset, val_dataset= data.split(rawdata,rate=0.8,shuffle=False,aging=config['circuit']['aging'], \
-                              agestep=config['circuit']['agestep'])
+    train_dataset, val_dataset= data.split(rawdata, rate=0.8, shuffle=False, aging=config['circuit']['aging'], \
+                                agestep=config['circuit']['agestep'])
 
-data_module = CircuitDataModule(train_dataset=train_dataset, val_dataset=val_dataset, batch_size=32, num_workers=4)
+    datamodule = CircuitDataModule(train_dataset=train_dataset, val_dataset=val_dataset, batch_size=32, num_workers=4)
 
-crossformer_model = Crossformer(data_dim=3, in_len=500, out_len=500, seg_len=6, win_size=4,
-                factor=10, d_model=512, d_ff=1024, n_heads=8, e_layers=3,
-                dropout=0.0, baseline = False, device=torch.device('cuda:0'))
+    datamodule.setup()
+    train_loader = datamodule.train_dataloader()
+    val_loader = datamodule.val_dataloader()
 
-trainer = Trainer(
-    max_epochs = config['training']['maxepoch'],
-    # accelerator = 'auto',
-    # accelerator = 'gpu' if torch.cuda.is_available() else 'cpu',
-    # devices = 1 if torch.cuda.is_available() else None,
-    precision= 32,
-    # precision=16 if config['training']['mixed_precision'] else 32,
-)
+    crossformer_model = CrossformerLightningModule(data_dim=3, in_len=500, out_len=500, seg_len=6, win_size=4,
+                    factor=10, d_model=512, d_ff=1024, n_heads=8, e_layers=3,
+                    dropout=0.0, baseline = False) #, device=torch.device('cuda:0'))
 
-trainer.fit(crossformer_model, datamodule=data_module)
+    trainer = Trainer(
+        max_epochs = config['training']['maxepoch'],
+        precision= 32,
+        accelerator='gpu' if torch.cuda.is_available() else 'cpu',
+        devices = 1,
+        # devices = 1 if torch.cuda.is_available() else None,
+        # precision=16 if config['training']['mixed_precision'] else 32,
+    )
+
+    trainer.fit(model=crossformer_model, datamodule=datamodule)
+    print("Training finished!")
