302f7b0f18d8d76b1760713cc8876ca4d7221e21
Yi-Chia Chang <yichia3@illinois.edu>
2024-10-24 22:39:23 -0500

    update script

--- scripts/cyp.sh
+++ scripts/cyp.sh
@@ -7,8 +7,16 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=32
 #SBATCH --gres=gpu:A100:1
+#SBATCH --output=.experiment-logs/o%j
+#SBATCH --error=.experiment-logs/e%j
+#SBATCH --mail-user=yichia3@illinois.edu
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
 
+module load anaconda/2023-Mar/3
+source activate
 conda activate dali
+
 cd ~/Crossformer
 
 python3 run_cyp.py
